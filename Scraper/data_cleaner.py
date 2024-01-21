0890d41f5246220848148b107bd12faaca454af5
Kushwaha <kushwaha.priyanshu.imp@gmail.com>
2024-01-21 06:18:21 +0530

    Cleaned code and updated structure

--- Scraper/data_cleaner.py
+++ Scraper/data_cleaner.py
@@ -1,24 +1,29 @@
 import pandas as pd
-    
-def main():
 
+def main():
+    # Read existing filtered data if it exists
+    try:
+        existing_data = pd.read_csv('filtered_data.csv')
+    except FileNotFoundError:
+        existing_data = pd.DataFrame()
+
+    # Read new data
     csv = pd.read_csv('google_maps_data.csv')
     df = pd.DataFrame(csv)
 
-    df = df.drop('name', axis=1)
-    df = df.drop('website', axis=1)
-    df = df.drop('latitude', axis=1)
-    df = df.drop('longitude', axis=1)
-    df = df.drop('reviews_average', axis=1)
-    df = df.drop('reviews_count', axis=1)
-    df = df.drop('address', axis=1)
+    # Perform filtering operations
+    df = df.drop(['name', 'website', 'latitude', 'longitude', 'reviews_average', 'reviews_count', 'address'], axis=1)
     df = df[df['phone_number'].notna()]
     pattern = r"\b\d{11}\b|\b\d{6} \d{5}\b"
     df = df[df['phone_number'].str.contains(pattern, na=False)]
 
-    df.to_csv('filtered_data.csv', index=False)
+    # Combine existing data with new filtered data
+    df_combined = pd.concat([existing_data, df], ignore_index=True)
 
-    print(df)
+    # Save the combined data to the CSV file
+    df_combined.to_csv('filtered_data.csv', index=False)
+
+    print(df_combined)
 
 if __name__ == "__main__":
     main()
